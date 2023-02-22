258ec0f0bb434805d21c4b81099eca67f813f28b
Peroski009 <Osarumwensepeterson@gmail.com>
2023-02-21 17:40:58 -0800

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,6 +4,7 @@
  * Return: empty output
  */
 void times_table(void)
+
 {
 
 	int x, y, z, u, d;
@@ -18,7 +19,7 @@ void times_table(void)
 	u = z % 10;
 	d = (z - u) / 10;
 	_putchar(44);
-	_putchar(12);
+	_putchar(32);
 	_putchar(d + '0');
 	_putchar(u + '0');
 	}
