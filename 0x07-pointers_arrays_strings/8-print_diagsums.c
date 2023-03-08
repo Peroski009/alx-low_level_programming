4f7ae3de46a75e45e1ea7aadb6922d08f7ff9c30
Peroski009 <Osarumwensepeterson@gmail.com>
2023-03-07 16:20:31 -0800

    8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_diagsums - Entry point
@@ -17,4 +17,4 @@ void print_diagsums(int *a, int size)
 	for (n = size - 1; n <= (size * size) - size; n = n + size - 1)
 	sum2 = sum2 + a[n];
 	printf("%d, %d\n", sum1, sum2);
-
+}
